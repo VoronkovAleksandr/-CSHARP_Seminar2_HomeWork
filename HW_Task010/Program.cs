66d1826f6d90100efd591eece381aa6a44e44300
Aleksandr <a_voron@list.ru>
2022-12-04 14:58:32 +0500

    Task010 completed

---
+++ HW_Task010/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное 
+// число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+Console.Write("Введите трёхзначное число: ");
+int number = int.Parse(Console.ReadLine());
+int result = (number / 10) % 10;
+Console.WriteLine(number + " -> " + result);
